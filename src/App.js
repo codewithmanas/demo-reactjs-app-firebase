15b639d2c4ac853f14d275d67e7e795887e63688
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-02-04 10:02:46 +0530

    some changes on page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Deployment is going on here.
-        <p>hosted on <code>Firebase</code></p>
+        <p>hosted on <code>Firebase</code> with GitHub Actions.</p>
       </header>
     </div>
   );
